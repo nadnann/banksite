cb853148b870ab4fe1f83c310f2dee0284e033de
Adnan Nacir <112492142+nadnann@users.noreply.github.com>
2026-09-09 15:58:52 +0300

    Update rector.php

--- rector.php
+++ rector.php
@@ -3,11 +3,34 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
 
 return RectorConfig::configure()
     ->withPaths([
-        __DIR__ . '/src', // Kodlarınızın bulunduğu klasörleri belirtin (örn: app, public, vb.)
+        __DIR__,
     ])
-    ->withPhpSets(
-        php85: true // PHP 7.2'den 8.5'e kadar olan tüm kuralları uygular
-    );
+    ->withSets([
+        LevelSetList::UP_TO_PHP_82,
+    ])
+    ->withSkip([
+        __DIR__ . '/.git',
+        __DIR__ . '/.github',
+        __DIR__ . '/.settings',
+        __DIR__ . '/.svn',
+        __DIR__ . '/_notes',
+        __DIR__ . '/_private',
+        __DIR__ . '/_vti_bin',
+        __DIR__ . '/_vti_cnf',
+        __DIR__ . '/_vti_log',
+        __DIR__ . '/_vti_pvt',
+        __DIR__ . '/_vti_txt',
+        __DIR__ . '/cgi-bin',
+        __DIR__ . '/css',
+        __DIR__ . '/css_menu',
+        __DIR__ . '/font',
+        __DIR__ . '/image',
+        __DIR__ . '/img_menu',
+        __DIR__ . '/jquery',
+        __DIR__ . '/psd',
+        __DIR__ . '/swf',
+    ]);
